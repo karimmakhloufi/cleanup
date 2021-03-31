1634019f989224290c8e2118e2a695d297d7112a
Karim Makhloufi <maclouf@gmail.com>
2021-03-31 02:46:27 +0200

    Event listener without useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [keys, setKeys] = useState([]);
+  const handleKeyDown = (event) => {
+    console.log(event);
+    setKeys([...keys, event.key]);
+  };
+  window.addEventListener("keydown", handleKeyDown);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {keys.map((key, index) => (
+          <li key={index}>{key}</li>
+        ))}
+      </ul>
     </div>
   );
 }
